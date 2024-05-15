aa7035a0884aca45872ecd1c67ffeb378ce5f8df
badu <batkhuu831@gmail.com>
2024-05-15 11:27:30 +0800

    commiting out file

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/me.app.web",
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
