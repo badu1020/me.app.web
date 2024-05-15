8fd191b58fc064569aceebd6e6ba651cbb478392
badu <batkhuu831@gmail.com>
2024-05-15 18:09:54 +0800

    testing 2

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -15,7 +15,7 @@ export function NavLinks() {
   ].map(([label, href], index) => (
     <Link
       key={label}
-      // target='{main}'
+      target='_blank'
       href={href}
       className="relative -mx-3 -my-2 rounded-lg px-3 py-2 text-sm text-white transition-colors delay-150 hover:text-gray-900 hover:delay-0"
       onMouseEnter={() => {
