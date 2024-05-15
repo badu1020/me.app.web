c78f07eccc8b859f3a7cc375643d801046826f38
badu <batkhuu831@gmail.com>
2024-05-15 12:06:37 +0800

    changes next Link to <a>

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -14,7 +14,7 @@ export function NavLinks() {
     ['Нэгдсэн системүүд', '/#integrated'],
     ['Түгээмэл асуултууд', '/FAQs']
   ].map(([label, href], index) => (
-    <Link
+    <a
       key={label}
       href={href}
       className="relative -mx-3 -my-2 rounded-lg px-3 py-2 text-sm text-white transition-colors delay-150 hover:text-gray-900 hover:delay-0"
@@ -45,6 +45,6 @@ export function NavLinks() {
         )}
       </AnimatePresence>
       <span className="relative z-10">{label}</span>
-    </Link>
+    </a>
   ))
 }
